19e647f74b22cc328b9cd075543b4605bddd9413
이호진 <kiss0104040@gmail.com>
2022-02-06 02:04:38 +0900

    #4.1 Test Drive part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-red-500 text-center flex'>
-      <h1 className='text-black'>it works</h1>
+    <div className=' bg-slate-400 py-20 px-4 grid gap-10'>
+      <div className='bg-white p-6 rounded-3xl shadow-2xl'>
+        <span className=' font-semibold text-3xl'>Select Item</span>
+        <div className='flex justify-between my-2'>
+          <span className='text-gray-500'>Gray Chair</span>
+          <span className='font-semibold'>$19</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className='text-gray-500'>Gray Chair</span>
+          <span className='font-semibold'>$19</span>
+        </div>
+        <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
+          <span>Total</span>
+          <span className='font-semibold'>$10</span>
+        </div>
+        <div className='mt-5 bg-blue-500 text-white p-2 text-center rounded-xl w-1/2 mx-auto'>
+          Checkout
+        </div>
+      </div>
+      <div className='bg-white p-10 rounded-2xl shadow-2xl'></div>
+      <div className='bg-white p-10 rounded-2xl shadow-2xl'></div>
+      <div className='bg-white p-10 rounded-2xl shadow-2xl'></div>
     </div>
   )
 }
