4c60e03e36359c6268b78d11bc149a4bec4a0980
이호진 <kiss0104040@gmail.com>
2022-02-08 19:30:09 +0900

    #5.0 Introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div className=' dark:md:hover:bg-teal-400'>
-      <h2 className="text-[99px] text-[#797979] bg-[url('/vercel.svg')]">
-        Hello
-      </h2>
-    </div>
-  )
+  return null
 }
 
 export default Home
