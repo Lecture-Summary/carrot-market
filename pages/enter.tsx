15ade8910582246acc4fc1d065ddc29bbfc469ce
이호진 <kiss0104040@gmail.com>
2022-02-17 00:43:05 +0900

    #5.18 Conclusions

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,23 +1,26 @@
+import type { NextPage } from 'next'
 import { useState } from 'react'
+import Button from '../components/button'
+import Input from '../components/input'
 import { cls } from '../libs/utils'
 
-export default function Enter() {
+const Enter: NextPage = () => {
   const [method, setMethod] = useState<'email' | 'phone'>('email')
   const onEmailClick = () => setMethod('email')
   const onPhoneClick = () => setMethod('phone')
   return (
     <div className='mt-16 px-4'>
       <h3 className='text-3xl font-bold text-center'>Enter to Carrot</h3>
-      <div className='mt-8'>
+      <div className='mt-12'>
         <div className='flex flex-col items-center'>
           <h5 className='text-sm text-gray-500 font-medium'>Enter using:</h5>
-          <div className='grid border-b w-full mt-8 grid-cols-2 gap-16'>
+          <div className='grid  border-b  w-full mt-8 grid-cols-2 '>
             <button
               className={cls(
-                'pb-4 font-medium border-b-2',
+                'pb-4 font-medium text-sm border-b-2',
                 method === 'email'
-                  ? 'border-orange-500 text-orange-400'
-                  : 'border-transparent text-gray-500'
+                  ? ' border-orange-500 text-orange-400'
+                  : 'border-transparent hover:text-gray-400 text-gray-500'
               )}
               onClick={onEmailClick}
             >
@@ -25,10 +28,10 @@ export default function Enter() {
             </button>
             <button
               className={cls(
-                'pb-4 font-medium border-b-2',
+                'pb-4 font-medium text-sm border-b-2',
                 method === 'phone'
-                  ? 'border-orange-500 text-orange-400'
-                  : 'border-transparent text-gray-500'
+                  ? ' border-orange-500 text-orange-400'
+                  : 'border-transparent hover:text-gray-400 text-gray-500'
               )}
               onClick={onPhoneClick}
             >
@@ -36,43 +39,29 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form className='flex flex-col mt-8'>
-          <label htmlFor='input' className='text-sm font-medium text-gray-700'>
-            {method === 'email' ? 'Email address' : null}
-            {method === 'phone' ? 'Phone number' : null}
-          </label>
-          <div className='mt-1'>
-            {method === 'email' ? (
-              <input
-                id='input'
-                type='email'
-                className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
-                required
-              />
-            ) : null}
-            {method === 'phone' ? (
-              <div className='flex rounded-sm shadow-sm'>
-                <span className='flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm'>
-                  +82
-                </span>
-                <input
-                  id='input'
-                  type='number'
-                  className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
-                  required
-                />
-              </div>
-            ) : null}
-          </div>
-          <button className='mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none'>
-            {method === 'email' ? 'Get login link' : null}
-            {method === 'phone' ? 'Get one-time password' : null}
-          </button>
+        <form className='flex flex-col mt-8 space-y-4'>
+          {method === 'email' ? (
+            <Input name='email' label='Email address' type='email' required />
+          ) : null}
+          {method === 'phone' ? (
+            <Input
+              name='phone'
+              label='Phone number'
+              type='number'
+              kind='phone'
+              required
+            />
+          ) : null}
+          {method === 'email' ? <Button text={'Get login link'} /> : null}
+          {method === 'phone' ? (
+            <Button text={'Get one-time password'} />
+          ) : null}
         </form>
+
         <div className='mt-8'>
           <div className='relative'>
             <div className='absolute w-full border-t border-gray-300' />
-            <div className='relative -top-3 text-center'>
+            <div className='relative -top-3 text-center '>
               <span className='bg-white px-2 text-sm text-gray-500'>
                 Or enter with
               </span>
@@ -109,3 +98,4 @@ export default function Enter() {
     </div>
   )
 }
+export default Enter
