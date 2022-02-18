6860eb6305628098316e81a50379439075c22d09
이호진 <kiss0104040@gmail.com>
2022-02-18 22:53:42 +0900

    #7.4 Validation part Two

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,26 +1,41 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { FieldErrors, useForm } from 'react-hook-form'
+
+interface LoginForm {
+  username: string
+  email: string
+  password: string
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm()
-  const onValid = () => {
+  const { register, handleSubmit } = useForm<LoginForm>()
+  const onValid = (data: LoginForm) => {
     console.log('valid')
   }
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors)
+  }
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register('username', { required: true })}
+        {...register('username', {
+          required: 'Username is required',
+          minLength: {
+            value: 5,
+            message: 'The username should be longer than 5 chars.',
+          },
+        })}
         type='text'
         placeholder='Username'
         minLength={5}
       />
       <input
-        {...register('email', { required: true })}
+        {...register('email', { required: 'Email is required' })}
         type='email'
         placeholder='Email'
       />
       <input
-        {...register('password', { required: true })}
+        {...register('password', { required: 'Password is required' })}
         type='password'
         placeholder='Password'
       />
