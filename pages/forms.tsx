5c2a375c7a03931ee11cf26d07df4e177f0f04ca
이호진 <kiss0104040@gmail.com>
2022-02-18 23:02:08 +0900

    #7.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -8,7 +8,11 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: 'onChange' })
   const onValid = (data: LoginForm) => {
     console.log('valid')
   }
@@ -30,10 +34,18 @@ export default function Forms() {
         minLength={5}
       />
       <input
-        {...register('email', { required: 'Email is required' })}
+        {...register('email', {
+          required: 'Email is required',
+          validate: {
+            notGmail: (value) =>
+              !value.includes('@gmail.com') || 'Gmail is not allowed',
+          },
+        })}
         type='email'
         placeholder='Email'
+        className={`${Boolean(errors.email?.message) && 'border-red-500'}`}
       />
+      {errors.email?.message}
       <input
         {...register('password', { required: 'Password is required' })}
         type='password'
