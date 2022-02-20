1cfd38fd269e87bb3cf4efec9c9daf80398c8796
이호진 <kiss0104040@gmail.com>
2022-02-21 00:32:50 +0900

    #8.3 Clean Code part Two

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 
-export default function useMutation(
-  url: string
-): [
-  (data: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
+interface UseMutationState {
+  loading: boolean
+  data?: object
+  error?: object
+}
+type UseMutationResult = [(data: any) => void, UseMutationState]
+
+export default function useMutation(url: string): UseMutationResult {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  })
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState<undefined | any>(undefined)
   const [error, setError] = useState<undefined | any>(undefined)
-  function mutation(data?: any) {}
+  function mutation(data?: any) {
+    setLoading(true)
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json().catch(() => {}))
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false))
+  }
   return [mutation, { loading, data, error }]
 }
