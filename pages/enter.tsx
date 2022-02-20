34baae9deb128d50b10de2dc31529bf83bd9c011
이호진 <kiss0104040@gmail.com>
2022-02-21 00:10:58 +0900

    #8.1 Form Submission

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -11,6 +11,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+  const [submitting, setSubmitting] = useState(false)
   const { register, handleSubmit, reset } = useForm<EnterForm>()
   const [method, setMethod] = useState<'email' | 'phone'>('email')
   const onEmailClick = () => {
@@ -22,7 +23,14 @@ const Enter: NextPage = () => {
     setMethod('phone')
   }
   const onValid = (data: EnterForm) => {
-    console.log(data)
+    setSubmitting(true)
+    fetch('/api/users/enter', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(() => {
+      setSubmitting(false)
+    })
   }
 
   return (
@@ -81,7 +89,7 @@ const Enter: NextPage = () => {
           ) : null}
           {method === 'email' ? <Button text={'Get login link'} /> : null}
           {method === 'phone' ? (
-            <Button text={'Get one-time password'} />
+            <Button text={submitting ? 'Loading' : 'Get one-time password'} />
           ) : null}
         </form>
 
