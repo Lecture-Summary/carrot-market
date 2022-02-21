b99e4302faa824b8e8dd330f1df2b61b0fddbe57
이호진 <kiss0104040@gmail.com>
2022-02-22 00:22:16 +0900

    #9.2 Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,12 +5,18 @@ import { NextApiRequest, NextApiResponse } from 'next'
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body
   const payload = phone ? { phone: +phone } : { email }
-  const user = await client.user.upsert({
-    where: { ...payload },
-    create: { name: 'Anonymous', ...payload },
-    update: {},
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        connectOrCreate: {
+          where: { ...payload },
+          create: { name: 'Anonymous', ...payload },
+        },
+      },
+    },
   })
-  console.log(user)
+  console.log(token)
   return res.status(200).end()
 }
 
