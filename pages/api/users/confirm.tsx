0aceeefd52a04c17a9b5727ca58b143261f8a299
이호진 <kiss0104040@gmail.com>
2022-02-22 23:46:35 +0900

    #9.7 Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,13 +1,23 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
 import withHandler, { ResponseType } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
+import client from '@libs/server/client'
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body
-  console.log(token)
+  const exists = await client.token.findUnique({ where: { payload: token } })
+  if (!exists) res.status(404).end()
+  req.session.user = {
+    id: exists?.userId,
+  }
+  await req.session.save()
   res.end()
 }
 
-export default withHandler('POST', handler)
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrotsession',
+  password: '780adfs7890789078ds90789asdf78dfs9089ds7fd890f89ddd',
+})
