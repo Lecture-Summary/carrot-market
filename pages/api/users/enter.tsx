02ad5e3faafa7d986b9fabd89da52007fe12e015
이호진 <kiss0104040@gmail.com>
2022-02-22 21:27:12 +0900

    #9.3 Twilio Setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,23 +1,27 @@
 import client from '@libs/server/client'
-import withHandler from '@libs/server/withHandler'
+import withHandler, { ResponseType } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body
-  const payload = phone ? { phone: +phone } : { email }
+  const user = phone ? { phone: +phone } : email ? { email } : null
+  if (!user) return res.status(400).json({ ok: false })
+  const payload = Math.floor(100000 + Math.random() * 900000) + ''
   const token = await client.token.create({
     data: {
-      payload: '1234',
+      payload,
       user: {
         connectOrCreate: {
-          where: { ...payload },
-          create: { name: 'Anonymous', ...payload },
+          where: { ...user },
+          create: { name: 'Anonymous', ...user },
         },
       },
     },
   })
-  console.log(token)
-  return res.status(200).end()
+  return res.json({ ok: true })
 }
 
 export default withHandler('POST', handler)
