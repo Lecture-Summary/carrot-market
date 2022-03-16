2e49487a54f799ba9047d0d077d25d232b7be841
이호진 <kiss0104040@gmail.com>
2022-03-16 23:22:22 +0900

    #11.7 Favorite Products part Two

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query
+  const {
+    query: { id },
+    session: { user },
+  } = req
   const product = await client.product.findUnique({
     where: { id: +id.toString() },
     include: { user: { select: { id: true, name: true, avatar: true } } },
@@ -18,7 +21,13 @@ async function handler(
   const relatedProducts = await client.product.findMany({
     where: { OR: terms, AND: { id: { not: product?.id } } },
   })
-  res.json({ ok: true, product, relatedProducts })
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: { productId: product?.id, userId: user?.id },
+      select: { id: true },
+    })
+  )
+  res.json({ ok: true, product, isLiked, relatedProducts })
 }
 
 export default withApiSession(withHandler({ methods: ['GET'], handler }))
