92486e8180e3e54bc3f0937ef4337c727ab1b087
이호진 <kiss0104040@gmail.com>
2022-03-16 23:34:28 +0900

    11.8 Bound Mutations

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -21,11 +21,13 @@ interface ItemDetailResponse {
 
 const ItemDetail: NextPage = () => {
   const router = useRouter()
-  const { data } = useSWR<ItemDetailResponse>(
+  const { data, mutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   )
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`)
   const onFavClick = () => {
+    if (!data) return
+    mutate({ ...data, isLiked: !data.isLiked }, false)
     toggleFav({})
   }
   return (
