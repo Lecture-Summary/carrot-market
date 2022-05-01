39057a6a58ae93a201210f7aebd2cbf6390ccafa
이호진 <kiss0104040@gmail.com>
2022-05-01 21:09:10 +0900

    #12.3 궁금해요

--- pages/api/posts/[id]/index.ts
+++ pages/api/posts/[id]/index.ts
@@ -9,6 +9,7 @@ async function handler(
 ) {
   const {
     query: { id },
+    session: { user },
   } = req
   const post = await client.post.findUnique({
     where: { id: +id.toString() },
@@ -18,24 +19,19 @@ async function handler(
         select: {
           id: true,
           answer: true,
-          user: {
-            select: {
-              id: true,
-              name: true,
-              avatar: true,
-            },
-          },
+          user: { select: { id: true, name: true, avatar: true } },
         },
       },
-      _count: {
-        select: {
-          answers: true,
-          wondering: true,
-        },
-      },
+      _count: { select: { answers: true, wondering: true } },
     },
   })
-  res.json({ ok: true, post })
+  const isWondering = Boolean(
+    await client.wondering.findFirst({
+      where: { postId: +id.toString(), userId: user?.id },
+      select: { id: true },
+    })
+  )
+  res.json({ ok: true, post, isWondering })
 }
 
 export default withApiSession(withHandler({ methods: ['GET'], handler }))
---
+++ pages/api/posts/[id]/wonder.ts
@@ -0,0 +1,30 @@
+import withHandler, { ResponseType } from '@libs/server/withHandler'
+import { NextApiRequest, NextApiResponse } from 'next'
+import client from '@libs/server/client'
+import { withApiSession } from '@libs/server/withSession'
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req
+  const alreadyExists = await client.wondering.findFirst({
+    where: { userId: user?.id, postId: +id.toString() },
+    select: { id: true },
+  })
+  if (alreadyExists) {
+    await client.wondering.delete({ where: { id: alreadyExists.id } })
+  } else {
+    await client.wondering.create({
+      data: {
+        user: { connect: { id: user?.id } },
+        post: { connect: { id: +id.toString() } },
+      },
+    })
+  }
+}
+
+export default withApiSession(withHandler({ methods: ['POST'], handler }))
