0ef047288db127cc088dcd7975f10860bc25f6cb
이호진 <kiss0104040@gmail.com>
2022-05-05 18:12:53 +0900

    #13.1 Reviews

---
+++ pages/api/reviews.ts
@@ -0,0 +1,20 @@
+import withHandler, { ResponseType } from '@libs/server/withHandler'
+import { NextApiRequest, NextApiResponse } from 'next'
+import client from '@libs/server/client'
+import { withApiSession } from '@libs/server/withSession'
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req
+  const reviews = await client.review.findMany({
+    where: { createdForId: user?.id },
+    include: { createdBy: { select: { id: true, name: true, avatar: true } } },
+  })
+  res.json({ ok: true, reviews })
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }))
