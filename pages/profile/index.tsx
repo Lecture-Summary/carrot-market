e62fcacd2c7e87e3a2075dae5bf35445ad3a5842
이호진 <kiss0104040@gmail.com>
2022-05-29 21:37:33 +0900

    #19.8 SSR + Authentication

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from 'next'
+import type { NextPage, NextPageContext } from 'next'
 import Link from 'next/link'
 import Layout from '@components/layout'
 import useUser from '@libs/client/useUser'
-import useSWR from 'swr'
+import useSWR, { SWRConfig } from 'swr'
 import { Review, User } from '@prisma/client'
 import { cls } from '@libs/client/utils'
+import { withSsrSession } from '@libs/server/withSession'
+import client from '@libs/server/client'
 
 interface ReviewWithUser extends Review {
   createdBy: User
@@ -18,7 +20,7 @@ const Profile: NextPage = () => {
   const { user } = useUser()
   const { data } = useSWR<ReviewsResponse>('/api/reviews')
   return (
-    <Layout hasTabBar title='나의 캐럿'>
+    <Layout hasTabBar title='나의 캐럿' seoTitle={user?.name}>
       <div className='px-4'>
         <div className='flex items-center mt-4 space-x-3'>
           {user?.avatar ? (
@@ -146,4 +148,27 @@ const Profile: NextPage = () => {
   )
 }
 
-export default Profile
+const Page: NextPage<{ profile: User }> = ({ profile }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          '/api/users/me': { ok: true, profile },
+        },
+      }}
+    >
+      <Profile />
+    </SWRConfig>
+  )
+}
+
+export const getServerSideProps = withSsrSession(async function ({
+  req,
+}: NextPageContext) {
+  const profile = await client.user.findUnique({
+    where: { id: req?.session.user?.id },
+  })
+  return { props: { profile: JSON.parse(JSON.stringify(profile)) } }
+})
+
+export default Page
