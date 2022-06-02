1ee763387abba488c0e93d42178651559dd8225b
이호진 <kiss0104040@gmail.com>
2022-06-02 23:22:17 +0900

    #21.3 Throwing Our Promise

---
+++ pages/coins.server.tsx
@@ -0,0 +1,36 @@
+import { Suspense } from 'react'
+
+const cache: any = {}
+function fetchData(url: string) {
+  if (!cache[url]) {
+    throw fetch(url)
+      .then((r) => r.json())
+      .then((json) => (cache[url] = json.slice(0, 50)))
+  }
+  return cache[url]
+}
+
+function List() {
+  const coins = fetchData('https://api.coinpaprika.com/v1/coins')
+  console.log(coins)
+  return (
+    <ul>
+      {coins.map((coin: any) => (
+        <li key={coin.id}>
+          {coin.name} / {coin.symbol}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default function Coins() {
+  return (
+    <div>
+      <h1>Welcome to RSC</h1>
+      <Suspense fallback='Rendering in the server'>
+        <List />
+      </Suspense>
+    </div>
+  )
+}
